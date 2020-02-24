eb2136b696c475c8d9bb28c35909049dbe612eab
PJ-Steeman <pieterjan.steeman@gmail.com>
2020-02-24 12:02:06 +0100

    listList
    RIP listList (in our harts forever)

--- heuristiek.py
+++ heuristiek.py
@@ -1,15 +1,27 @@
 # Jelle Caerlen, Kwinten Vanlathem en Pieter-Jan Steeman
 
-#TODO: lees aantal dagen in en check of er niet buiten gereserveerd wordt
+#TODO:  lees aantal dagen in en check of er niet buiten gereserveerd wordt
+#       Seed voor random number generator
+#       Enters in de output file
+
+import sys
+import time
 
 class Zone:
     def __init__(self, line):
-        self.name = line[0]
+        self.id = line[0]
         self.adj = line[1].split(",")
+
+    def __str__(self):
+        return self.id
 
 class Vehicle:
     def __init__(self, line):
-        self.name = line[0]
+        self.id = line[0]
+        self.zone = "huijjiunuib"
+
+    def __str__(self):
+        return self.id
 
     def setZone(self, zone):
         self.zone = zone
@@ -18,38 +30,46 @@ class Reservation:
     def __init__(self, line):
         self.id = line[0]
         self.zone = line[1]
-        self.day = line[2]
-        self.start = line[3]
-        self.lenght = line[4]
+        self.day = int(line[2])
+        self.start = int(line[3])
+        self.lenght = int(line[4])
         self.vehicles = line[5].split(",")
-        self.p1 = line[6]
-        self.p2 = line[7]
+        self.p1 = int(line[6])
+        self.p2 = int(line[7])
+        self.assigned_veh = None
+
+    def __str__(self):
+        return self.id
 
     def setVehicle(self, vehicle):
         self.assigned_veh = vehicle
 
     def calcCost(self):
-        if(checkSet):
+        print(self.checkSet())
+        print("----------")
+        if(self.checkSet()):
             if(self.zone == self.assigned_veh.zone):
                 return 0
             else:
-                return p2
-        return p1
+                return self.p2
+        return self.p1
 
     def checkSet(self):
         # OPGEPAST BIJ VERKEERD OUTPUT
         return self.assigned_veh is not None
 
-def calculateCost(self, resList):
+def calculateCost(resList):
     cost = 0
     for res in resList:
         cost += res.calcCost()
     return cost
 
-def readFile(self, path):
-    reslist = []
+def readFile(path):
+    resList = []
     zoneList = []
     vehList = []
+
+    print("ok")
 
     listIdent = -1
 
@@ -60,19 +80,59 @@ def readFile(self, path):
              listIdent += 1
          else:
             if(listIdent == 0):
-                reslist.append(Reservation(line.split(";")))
+                resList.append(Reservation(line.split(";")))
             if(listIdent == 1):
-                zonelist.append(Zone(line.split(";")))
+                zoneList.append(Zone(line.split(";")))
             if(listIdent == 2):
-                vehlist.append(Vehicle(line.split(";")))
+                vehList.append(Vehicle(line.split(";")))
 
     file.close()
-    return resList, zoneList, vehList
+    return vehList, zoneList, resList
+
+def writeFile(path, vehicles, reservations):
+    file = open(path, "w")
+
+    file.write(str(calculateCost(reservations)))
+    file.write("+Vehicle assignments")
+    for veh in vehicles:
+        file.write(str(veh) + ";" + veh.zone)
+    file.write("+Assigned requests")
+    for res in reservations:
+        if(res.checkSet()):
+            file.write(str(res) + ";" + res.assigned_veh)
+    file.write("+Unassigned requests")
+    for res in reservations:
+        if(not res.checkSet()):
+            file.write(str(res))
+    file.close()
 
 def main():
-    path = "bleh.csv"
+    pathIn = sys.argv[1]
+    pathOut = sys.argv[2]
+    maxTime = int(sys.argv[3])
+
+    print(pathIn)
+    print(pathOut)
+    print(maxTime)
+
     listVeh = []
     listZone = []
     listRes = []
 
-    listVeh, listZone, listRes = readFile(path)
+    listVeh, listZone, listRes = readFile(pathIn)
+
+    print(listVeh[1])
+
+    start_time = time.time()
+
+    while(time.time() - start_time) < maxTime:
+        # OPTIMALISTAIE
+        print("ok")
+
+    #print(listVeh)
+    #print(listZone)
+    #print(listRes)
+
+    writeFile(pathOut, listVeh, listRes)
+
+main()
