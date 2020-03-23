22184fa1b01c691694ee13a8e6ba8986425d4605
Pieter-Jan Steeman <pieterjan.steeman@gmail.com>
2020-03-23 09:11:42 +0100

    rip listList

--- heuristiek.py
+++ heuristiek.py
@@ -8,11 +8,15 @@ import time
 import random
 
 class Zone:
-    def __init__(self, line):
-        self.id = line[0]
-        self.adj = line[1].split(",")
+    def __init__(self):
+        self.id = None
+        self.adj = None
         self.veh = []
         self.vehNeigh = []
+
+    def setInit(self, id, adjList):
+        self.id = id
+        self.adj = adjList
 
     def setVeh(self, list):
         self.veh = list
@@ -24,18 +28,31 @@ class Zone:
         return self.id
 
 class Vehicle:
-    def __init__(self, line):
+    def __init__(self):
+        self.id = None
+        self.zone = None
+
+    def setInit(self, line):
         self.id = line[0]
-        self.zone = "test zone"
 
     def __str__(self):
         return self.id
 
     def setZone(self, zone):
-        self.zone = str(zone)
+        self.zone = zone
 
 class Reservation:
-    def __init__(self, line):
+    def __init__(self):
+        self.id = None
+        self.zone = None
+        self.day = None
+        self.start = None
+        self.lenght = None
+        self.vehicles = None
+        self.p1 = None
+        self.p2 = None
+
+    def setInit(self, line):
         self.id = line[0]
         self.zone = line[1]
         self.day = int(line[2])
@@ -80,19 +97,48 @@ def readFile(path):
     print("ok")
 
     listIdent = -1
-
+    follower = 0
     file = open(path, "r")
 
     for line in file:
-         if(line[0] == "+"):
-             listIdent += 1
-         else:
+        if(line[0] == "+"):
+            listIdent += 1
+            follower = 0
+
+            parts = line.split(": ")
             if(listIdent == 0):
-                resList.append(Reservation(line.split(";")))
+                for i in range(int(parts[1])):
+                    resList.append(Reservation())
             if(listIdent == 1):
-                zoneList.append(Zone(line.split(";")))
+                for i in range(int(parts[1])):
+                    zoneList.append(Zone())
             if(listIdent == 2):
-                vehList.append(Vehicle(line.split(";")))
+                for i in range(int(parts[1])):
+                    vehList.append(Vehicle())
+
+        else:
+            if(listIdent == 0):
+                resList[follower].setInit(line.split(";"))
+
+            if(listIdent == 1):
+                splitLine = line.split(";")
+                splitItems = splitLine[1].split(",")
+                place = "".join(splitItems)
+                zones = place.split("z")[1:]
+                tempList = []
+                for zone in zones:
+                    tempList.append(zoneList[int(zone)])
+
+                zoneList[follower].setInit(splitLine[0], tempList)
+
+            if(listIdent == 2):
+                vehList[follower].setInit(line.split(";"))
+            follower += 1
+
+    for res in resList:
+        res.zone = getItem(res.zone, zoneList)
+        for id, veh in enumerate(res.vehicles):
+            res.vehicles[id] = getItem(veh, vehList)
 
     file.close()
     return vehList, zoneList, resList
@@ -103,11 +149,11 @@ def writeFile(path, vehicles, reservations):
     file.write(str(calculateCost(reservations)) + "\n")
     file.write("+Vehicle assignments\n")
     for veh in vehicles:
-        file.write(str(veh).rstrip() + ";" + veh.zone + "\n")
+        file.write(str(veh).rstrip() + ";" + str(veh.zone) + "\n")
     file.write("+Assigned requests\n")
     for res in reservations:
         if(res.checkSet()):
-            file.write(str(res).rstrip() + ";" + res.assigned_veh + "")
+            file.write(str(res).rstrip() + ";" + str(res.assigned_veh) + "")
     file.write("+Unassigned requests\n")
     for res in reservations:
         if(not res.checkSet()):
@@ -116,18 +162,49 @@ def writeFile(path, vehicles, reservations):
 
 def randomZoneAssignment(listVeh, listZone):
     for veh in listVeh:
-        veh.setZone("z" + str(random.randrange(0, len(listZone))))
+        veh.setZone(listZone[random.randrange(0, len(listZone))])
         print(veh.zone)
 
-# def requestAssignment(listVeh, listZone, listRes):
-    # for req in listRes
+def requestAssignment(listZone, listRes):
+    request = listRes[random.randrange(0, len(listRes))]
 
-# def checkCarFree():
+    found = False
+
+    for veh in random.shuffle(request.zone.veh):
+        if(checkCarAvailable(car, listRes, req)):
+            request.setVehicle(veh)
+            found = True
+            break
+
+    if not found:
+        for veh in random.shuffle(request.zone.vehNeigh):
+            if(checkCarAvailable(car, listRes, req)):
+                request.setVehicle(veh)
+                break
+
+def checkCarAvailable(veh, listRes, req):
+    if (veh not in req.vehicles):
+        return False
+    vehRange = range(req.start, req.start + req.length)
+    for fixed in listRes:
+        if (veh == fixed.assigned_veh):
+            if (req.day != fixed.day):
+                return True
+            fixedRange = range(fixed.start, fixed.start + fixed.length)
+            if (list(set(vehRange) & list(set(fixedRange))) > 1):
+                return False
+            #if (req.start < fixed.start + fixed.lenght) and (req.start + req.lenght > fixed.start):
+            #    return False
+            #if (req.start + req.lenght > fixed.start) and (req.start + req.lenght < fixed.start + fixed.lenght):
+            #    return False
+            #if (req.start <= fixed.start) and (req.start+ req.lenght > fixed.start + fixed.lenght):
+            #    return False
+    return True
 
 def getVehicleInZone(zone, listVeh):
     listVehInZone = []
     for veh in listVeh:
-        if(veh.zone == str(zone)):
+        if(veh.zone == zone):
             listVehInZone.append(veh)
     return listVehInZone
 
@@ -156,10 +233,6 @@ def main():
     print(pathOut)
     print(maxTime)
 
-    listVeh = []
-    listZone = []
-    listRes = []
-
     listVeh, listZone, listRes = readFile(pathIn)
 
     start_time = time.time()
@@ -168,10 +241,10 @@ def main():
 
     for zone in listZone:
         zone.setVeh(getVehicleInZone(zone, listVeh))
+        # print(len(zone.veh))
     for zone in listZone:
         zone.setVehNeigh(getVehicleInNeighbour(zone))
-
-    print(listVeh[1])
+        # print(len(zone.vehNeigh))
 
     while(time.time() - start_time) < maxTime:
         # OPTIMALISTAIE
